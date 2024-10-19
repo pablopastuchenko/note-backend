10375cd9b653d19ef15e9a488bd58fe065a45096
pastuchenko <pablopastuchenkooo@gmail.com>
2024-10-19 11:47:27 -0300

    first commit

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const app = express()
 // to make input as json
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors({ origin: ["http://localhost:3000"], credentials: true }))
+app.use(cors({ origin: ["http://localhost:5173"], credentials: true }))
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000")
